c52e614cfa79d2953e10ae0c42a7d283c8ab5acd
Bruce Hopenfeld <85252234+Hopenfeld@users.noreply.github.com>
2021-06-15 08:48:17 -0600

    Add files via upload

--- meshsegs.m
+++ meshsegs.m
@@ -1,11 +1,12 @@
 
-function [scoresn,rrso] = meshsegs(tmsnxt,tmscur,rrs,scores,scoresnxt)
+function [scoresn,nsa] = meshsegs(tmsnxt,tmscur,rrsnxt,rrscur,scores,scoresnxt)
   %if the last part of a sequence from the previous segment (tmslast) temporally 
   %meshes with the start of a sequence from the current segment (tmscur)
   %add a fraction of the previous sequence's score to the present sequence's score
   
   scoresn=scores;
   rrso=zeros(1,length(tmscur(:,1)));
+  nsa=zeros(length(tmsnxt(:,1)),1);
 
     for jj=1:length(tmscur(:,1))
       ns=0;
@@ -15,8 +16,8 @@ function [scoresn,rrso] = meshsegs(tmsnxt,tmscur,rrs,scores,scoresnxt)
         continue
       end
       
-      rrn=median(tmb(2:end)-tmb(1:end-1));
-
+        rrn=rrscur(jj);
+        
       for ii=1:length(tmsnxt(:,1)) 
         tma=tmsnxt(ii,find(tmsnxt(ii,:)))+5000;
         tma=tma(find(tma<7500));
@@ -24,25 +25,31 @@ function [scoresn,rrso] = meshsegs(tmsnxt,tmscur,rrs,scores,scoresnxt)
           continue
         end
         seql=[tmb tma];
-        rrd=median(tma(2:end)-tma(1:end-1));
+
+        rrd=rrsnxt(ii);
         slst=scoresnxt(ii);
         if abs(rrn - rrd) > 200
           continue
         end
-        rrsm=rrd;
-        [regsc,rrl,skips,seql] = getmaxscore(seql,rrsm,1);
+        rrm=mean([rrn rrd]);
+        [regsc,rrl,skips,seql] = getmaxscore(seql,rrm,1);
         regsc=regsc/(length(seql)-2);
         ovap=~isempty(find(seql<=5000)) & ~isempty(find(seql >5000));  
         
         if  (regsc > -700 & ovap) 
-
-          ns=max(ns,int16(max(0,slst)*min(1,(rrsm+double(regsc))/350)));
+          
+          sfa=min(1,(rrm+double(regsc))/350);
+          sfb=max(0,(40-abs(rrn-rrd))/40);
+          ns=max(ns,int16(max(0,slst)*sfa));
+          ns=max(ns,int16(max(0,slst)*sfb));
+          
+          nsa(ii)=max(nsa(ii),int16(max(0,scores(jj))*sfa));
+          nsa(ii)=max(nsa(ii),int16(max(0,scores(jj))*sfb));
 
         end
 
       end
       scoresn(jj)=scoresn(jj)+ns;
-      rrso(jj)=rrn;
 
   end
   
